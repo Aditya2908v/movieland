e7deb6bdd2dcee74de6004a0256b596364109b67
aditya <adityareddy1451@gmail.com>
2023-07-08 20:44:16 +0530

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import React from "react";
-
+import { useEffect } from "react";
+import "./App.css";
+const API_URL = "http://www.omdbapi.com?apikey=8bdbe751";
+//API KEY 8bdbe751
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Hit");
+  }, []);
+
   return (
     <div>
       <h1>Hello</h1>
